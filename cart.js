e7d52b36b140b5912d404899a43125aa53394945
AugustovCruz <augusto.tito.96@gmail.com>
2025-01-10 06:57:41 -0300

    update final project

--- cart.js
+++ cart.js
@@ -2,6 +2,8 @@ let productsCarritoLS = JSON.parse(localStorage.getItem("productsCart"))
 const productCart = document.querySelector("#cart-products")
 let buttonDelet = document.querySelectorAll(".trash")
 
+const btnVoid = document.querySelector("#vaciar-carrito")
+
 
 
 function productosCarrito () {
@@ -15,18 +17,16 @@ function productosCarrito () {
                     <img src=${prod.image} alt="ropa">
                     <p>${prod.name}</p>
                     <section class="product-item">
-                        <p>Precio:</p>
-                        <p>Stock:</p>
+                        <p>Precio:${prod.maxKi}</p>
+                        <p>Stock:${prod.race}</p>
                     </section>
                     <section class="product-item">
                         <p>Cantidad</p>
                         <div class="btn-cart">
-                            <button>-</button> <span>1</span> <button>+</button>
+                            <button>-</button> <span>${prod.cantidad}</span> <button>+</button>
                         </div>
                     </section>
-                    <section class="trash">
-                        <button> 🗑 </button>
-                    </section>
+                    <button class="trash" id=${prod.id} > <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg> </button>                    
         `
         productCart.appendChild(div)
     });
@@ -35,11 +35,12 @@ function productosCarrito () {
 
 productosCarrito()
 
+
 function updateButtonDelet () {
     buttonDelet = document.querySelectorAll(".trash")
 
     buttonDelet.forEach(btn=> {
-        btn.addEvenListener("click", DeletToCart)
+        btn.addEventListener("click", DeletToCart)
     })
 }
 
@@ -47,4 +48,22 @@ function DeletToCart(e) {
     const idButton = e.currentTarget.id
     const productDelet = productsCarritoLS.find(prod => prod.id == idButton)
     console.log(productDelet)
+
+    const index = productsCarritoLS.findIndex(prod=> prod.id == idButton)
+    productsCarritoLS.splice(index, 1)
+    productosCarrito()
+
+    localStorage.setItem("productsCart", JSON.stringify(productsCarritoLS))
+    
 }
+
+btnVoid.addEventListener("click",voidCart)
+
+
+function voidCart () {
+    productsCarritoLS.length = 0
+    console.log(productsCarritoLS)
+    localStorage.setItem("productsCart",JSON.stringify(productsCarritoLS) )
+    productosCarrito()
+}
+
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
 const carrito = document.querySelector("#cart")
+const fondo = document.querySelector(".background")
 const abrir = document.querySelector("#button-cart")
 const cerrar = document.querySelector("#close")
 
@@ -10,15 +11,19 @@ const numberCart = document.querySelector("#number-cart")
 
 abrir.addEventListener("click", ()=> {
     carrito.classList.add("visible");
+    fondo.classList.add("background")
+    fondo.classList.add("background-visible")
 })
 
 cerrar.addEventListener("click", ()=> {
     carrito.classList.remove("visible")
+    fondo.classList.remove("background")
 })
 
 
 let totalProductos = []
 
+//Consumiendo el api y cargando los productos
 async function fetchData (url) {
     try {
         const response = await fetch(url)
@@ -38,24 +43,23 @@ fetchData("https://dragonball-api.com/api/characters")
 
 function productos (products) {
     
-    console.log("adentro de productos", products)
     offProducts.innerHTML= ""
     
     products.forEach(prod => {
         const div = document.createElement("section")
         div.classList.add("product")
         div.innerHTML= `
-                        <img src=${prod.image} alt="Producto 1">
+                        <img src=${prod.image} alt=${prod.name}>
                         <h3>${prod.name}</h3>
                         <p>${prod.ki}</p>
                         <button class="producto-agregar" id=${prod.id} >Añadir al Carrito</button>
         `
-        offProducts.appendChild(div)
+        offProducts.append(div)
     });
     actualizarBotones()
-    console.log(botonAdd)
 }
 
+//Actualizo los botones de agregar para que no se pierdan
 function actualizarBotones() {
     botonAdd = document.querySelectorAll(".producto-agregar")
     botonAdd.forEach( e=> {
@@ -63,23 +67,27 @@ function actualizarBotones() {
     })
 }
 
+//Verifico si hay products en el LS en caso contrario inicializo el carrito
 let productsCart
-const productsCartLS = JSON.parse(localStorage.getItem("productsCart"))
+const productsCartLS = JSON.parse(localStorage.getItem("productsCart")) 
 if (productsCartLS) {
     productsCart= productsCartLS
     updateNumber ()
-} else productsCart= []
+} else {
+    productsCart= []
+} 
 
 
 
+//Funcion para agregar al carrito en un nuevo array
 function addToCart(e) {
 
     const idBoton = e.currentTarget.id
-
+    
     const productAdd = totalProductos.find(prod => {
         return prod.id == idBoton
     })
-
+    
     if (productsCart.some(prod =>prod.id == idBoton)) { 
         const index = productsCart.findIndex(prod => prod.id == idBoton)
         productsCart[index].cantidad += 1
@@ -93,10 +101,29 @@ function addToCart(e) {
     localStorage.setItem("productsCart", JSON.stringify(productsCart))
 }
 
+//Controlo la cantidad de productos que se van sumando al carrito
 function updateNumber () {
     let number = productsCart.reduce((acc, producto)=> acc + producto.cantidad, 0)
     numberCart.innerHTML = number
 }
+
+//Manejo de categorias
+const buttonCategory = document.querySelectorAll(".boton-categoria")
+
+buttonCategory.forEach( btn=>{
+    btn.addEventListener("click", (e)=> {
+        //evitar que se recargue la pagina
+        e.preventDefault()
+        
+        if (e.currentTarget.id != "Todos") {
+            const productsCurrent = totalProductos.filter(prod => prod.race == e.currentTarget.id)
+            productos(productsCurrent)
+            actualizarBotones()
+        } else productos(totalProductos)
+    })
+})
+
+
 
 
 
